6a0b3874849ca88188296197452b4b0fe7c745d9
markus_hu <15055667366@qq.com>
2021-03-03 17:25:11 +0800

    first commit

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
+/*!******************!*\
+  !*** ./index.ts ***!
+  \******************/
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+// 检查开发环境是否为开发
+// 对整个App内进行生命周期钩子注入检查
+// 感觉需要在编译期进行检查，备选方案是做一个webpack的plugin
+// 其他方案是做一个vue的插件，根据官方暴露的接口进行检查
+// 面向扩展，此插件可能需要带日志收集功能
+var a = "";
+exports.default = a;
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly92dWUtdHJhY2stdG9vbHMvLi9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7QUFBYTtBQUNiLDhDQUE2QyxDQUFDLGNBQWMsRUFBQztBQUM3RDtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSxlQUFlIiwiZmlsZSI6ImluZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiXCJ1c2Ugc3RyaWN0XCI7XHJcbk9iamVjdC5kZWZpbmVQcm9wZXJ0eShleHBvcnRzLCBcIl9fZXNNb2R1bGVcIiwgeyB2YWx1ZTogdHJ1ZSB9KTtcclxuLy8g5qOA5p+l5byA5Y+R546v5aKD5piv5ZCm5Li65byA5Y+RXHJcbi8vIOWvueaVtOS4qkFwcOWGhei/m+ihjOeUn+WRveWRqOacn+mSqeWtkOazqOWFpeajgOafpVxyXG4vLyDmhJ/op4npnIDopoHlnKjnvJbor5HmnJ/ov5vooYzmo4Dmn6XvvIzlpIfpgInmlrnmoYjmmK/lgZrkuIDkuKp3ZWJwYWNr55qEcGx1Z2luXHJcbi8vIOWFtuS7luaWueahiOaYr+WBmuS4gOS4qnZ1ZeeahOaPkuS7tu+8jOagueaNruWumOaWueaatOmcsueahOaOpeWPo+i/m+ihjOajgOafpVxyXG4vLyDpnaLlkJHmianlsZXvvIzmraTmj5Lku7blj6/og73pnIDopoHluKbml6Xlv5fmlLbpm4blip/og71cclxudmFyIGEgPSBcIlwiO1xyXG5leHBvcnRzLmRlZmF1bHQgPSBhO1xyXG4iXSwic291cmNlUm9vdCI6IiJ9
